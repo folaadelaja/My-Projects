f3ab598ee10667bf15fd6963701446237db3718b
adeori2010 <104238747+adeori2010@users.noreply.github.com>
2022-06-22 11:00:36 -0400

    Added instructions

--- my_vpn.sh
+++ my_vpn.sh
@@ -1,3 +1,6 @@
-#Run my VPN
-openvpn /etc/openvpn/client/client.conf
+#!/bin/bash
+#Run my VPN config file
+#COPY YOUR OPENVPN CONFIG FILE IN THE DIRECTORY   /etc/openvpn/client/   AND RENAME THE FILE TO client.conf
+#RUN THE COMMAND BELOW AND LEAVE THE TERMINAL OPEN 
+sudo openvpn /etc/openvpn/client/client.conf
 
